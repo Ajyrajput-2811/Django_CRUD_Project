fa3a35a4d18097de032a1cbabba5ae1f97ef2bc6
Ajay Rajput <119350384+Ajyrajput-2811@users.noreply.github.com>
2023-05-25 21:08:07 +0530

    Update settings.py
    Changes done

--- CRUD_Application/settings.py
+++ CRUD_Application/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_nfl+&jrqime=qewc(*q!^(gsu@h!0ct987ml8+@z(vlv*lg$e'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 
 
 ALLOWED_HOSTS = []
 
